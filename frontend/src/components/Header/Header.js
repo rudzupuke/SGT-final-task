58e058df328ca2754ef1a715d3ad2416ef392b48
Your Name <m.braslava@gmail.com>
2022-04-01 19:07:41 +0300

    adding header elements

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,5 +1,23 @@
+import logo from '../../assets/logo.svg';
+import './Header.css';
+
 const Header = () => {
-	return <div className="header">Header</div>;
+	return (
+		<div className="header">
+			<div className="logo">
+				<img src={logo} className="logo__img" alt="logo"></img>
+				<span className="logo__text">Dinder</span>
+			</div>
+			<nav className="nav">
+				<ul className="nav__list">
+					<li className="nav__item">Home</li>
+					<li className="nav__item">About</li>
+					<li className="nav__item">Dashboard</li>
+				</ul>
+				<button className="button">Log in</button>
+			</nav>
+		</div>
+	);
 };
 
 export default Header;
