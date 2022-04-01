83685ae16e4a960cc9b542c7db61845db93c0938
Your Name <m.braslava@gmail.com>
2022-04-01 18:40:40 +0300

    creating a Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,22 @@ import data from './dummyData';
 import Home from './components/Home/Home';
 import Register from './components/Register/Register';
 import DashBoard from './components/DashBoard/DashBoard';
+import Header from './components/Header/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 	console.log(data);
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/dashboard" element={<DashBoard />} />
-				<Route path="/register" element={<Register />} />
-			</Routes>
-		</BrowserRouter>
+		<div className="app">
+			<Header />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/dashboard" element={<DashBoard />} />
+					<Route path="/register" element={<Register />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
 	);
 }
 
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+	return <div className="header">Header</div>;
+};
+
+export default Header;
