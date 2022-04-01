00158b9632cac74ed3812c2a71352e93baa97eda
Your Name <m.braslava@gmail.com>
2022-04-01 18:12:30 +0300

    deleting unnecessary curly braces

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,9 +10,9 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path={'/'} element={<Home />} />
-				<Route path={'/dashboard'} element={<DashBoard />} />
-				<Route path={'/register'} element={<Register />} />
+				<Route path="/" element={<Home />} />
+				<Route path="/dashboard" element={<DashBoard />} />
+				<Route path="/register" element={<Register />} />
 			</Routes>
 		</BrowserRouter>
 	);
