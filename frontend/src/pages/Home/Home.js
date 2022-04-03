a6713a7000f6d7c580a50b7139a3e0ab92e4ad2d
Your Name <m.braslava@gmail.com>
2022-04-03 22:49:48 +0300

    refactoring styles to scss

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,8 +1,14 @@
+import './Home.scss';
+import Header from "../../components/Header/Header"
+
 const Home = () => {
 	return (
-		<div className="Home">
-			<h1 className="heading--main">Find your dog a buddy!</h1>
-			<button className="button--primary">Create account &#8594;</button>
+		<div className="home">
+			<Header />
+			<div className="home__container">
+				<h1 className="heading--main">Find your dog a buddy!</h1>
+				<button className="button">Create account &#8594;</button>
+			</div>
 		</div>
 	);
 };
