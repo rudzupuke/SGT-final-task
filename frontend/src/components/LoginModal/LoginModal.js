a3adf337abc313b090722d3db7c8f0efc01efccf
rudzupuke <diana.gavare2@gmail.com>
2022-04-04 15:42:47 +0300

    install react-cookie and axios

--- frontend/src/components/LoginModal/LoginModal.js
+++ frontend/src/components/LoginModal/LoginModal.js
@@ -1,66 +1,55 @@
 import "./LoginModal.scss";
 import { useState } from "react";
+import axios from "axios";
+import { useCookie } from "react-cookie";
 
 const LoginModal = ({ setShowModal }) => {
-	const [email, setEmail] = useState(null);
-	const [password, setPassword] = useState(null);
-	const [confirmPassword, setConfirmPassword] = useState(null);
-	const [errors, setError] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [errors, setError] = useState(null);
 
-	console.log(email, password, confirmPassword);
+  console.log(email, password);
 
-	const handleCloseModal = () => {
-		setShowModal(false);
-	};
+  const handleCloseModal = async () => {
+    setShowModal(false);
+  };
 
-	const isNewUser = true;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-	};
-
-	return (
-		<div className="login-modal">
-			<button className="button--close" onClick={handleCloseModal}>
-				&#10006;
-			</button>
-			<h2>{isNewUser ? "CREATE ACCOUNT" : "LOG IN"}</h2>
-			<p>
-				By clicking Log In you agree to our Terms and conditions. Learn
-				more about our Privacy policy and Cookie policy
-			</p>
-			<form onSubmit={handleSubmit} className="login-modal__form">
-				<input
-					type="email"
-					id="email"
-					name="email"
-					placeholder="Email"
-					required
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<input
-					type="password"
-					id="password"
-					name="password"
-					placeholder="Password"
-					required
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<input
-					type="confirmPassword"
-					id="confirmPassword"
-					name="confirmPassword"
-					placeholder="Confirm password"
-					required
-					onChange={(e) => setConfirmPassword(e.target.value)}
-				/>
-				<button className="button--primary">
-					{isNewUser ? "CREATE ACCOUNT" : "LOG IN"}
-				</button>
-				<p className="error-message">{errors}</p>
-			</form>
-		</div>
-	);
+  return (
+    <div className="login-modal">
+      <button className="button--close" onClick={handleCloseModal}>
+        &#10006;
+      </button>
+      <h2>LOG IN</h2>
+      <p>
+        By clicking Log In you agree to our Terms and conditions. Learn more
+        about our Privacy policy and Cookie policy
+      </p>
+      <form onSubmit={handleSubmit} className="login-modal__form">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="button--primary">LOG IN</button>
+        <p className="error-message">{errors}</p>
+      </form>
+    </div>
+  );
 };
 
 export default LoginModal;
