e6f2f739d2d57551958e36e2ceb2ccd4a31c47c0
Mara Braslava <m.braslava@gmail.com>
2022-04-11 19:37:05 +0300

    responsive navbar on small screens only when logged in

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useCookies } from "react-cookie";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.svg";
@@ -6,6 +7,8 @@ import "./Header.scss";
 const Header = ({ authToken, setShowModal, showModal, user, isOnHomePage }) => {
     const [cookies, setCookie, removeCookie] = useCookies();
     const navigate = useNavigate();
+    const [navExpanded, setNavExpanded] = useState(false);
+
     const handleClick = () => {
         setShowModal(true);
     };
@@ -20,63 +23,66 @@ const Header = ({ authToken, setShowModal, showModal, user, isOnHomePage }) => {
 
     const toggleNav = () => {
         console.log("Nav toggled!");
+        setNavExpanded((prevNavExpanded) => !prevNavExpanded);
     };
 
     return (
         <header className={isOnHomePage ? "header" : "header header--loggedin"}>
-            <div className="container">
-                <div className="logo">
-                    <Link to="/">
-                        <img src={logo} className="logo__img" alt="logo"></img>
-                        <span className="logo__text">dinder</span>
-                    </Link>
-                </div>
-                <button
-                    aria-controls="primary-navigation"
-                    aria-expanded="false"
-                    class="nav-toggle"
-                    onClick={toggleNav}
-                >
-                    <span class="visually-hidden">Menu</span>
-                </button>
-                <nav className="nav" data-visible="false">
-                    <ul className="nav__list">
-                        <li className="nav__item">
-                            <Link to="/">Home</Link>
+            {/* <div className="container"> */}
+            <div className="logo">
+                <Link to="/">
+                    <img src={logo} className="logo__img" alt="logo"></img>
+                    <span className="logo__text">dinder</span>
+                </Link>
+            </div>
+            <button
+                aria-controls="primary-navigation"
+                aria-expanded={navExpanded}
+                className={authToken ? "nav-toggle" : "hidden"}
+                onClick={toggleNav}
+            >
+                <span className="visually-hidden">Menu</span>
+            </button>
+            <nav
+                className={authToken ? "nav nav--hamburger" : "nav"}
+                id="primary-navigation"
+                data-visible={navExpanded}
+            >
+                <ul className="nav__list">
+                    <li className="nav__item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    {/* <li className="nav__item">About</li> */}
+                    <li className="nav__item">
+                        {authToken && <Link to="/dashboard">Dashboard</Link>}
+                    </li>
+                    {!authToken && (
+                        <li>
+                            <button
+                                className="button--primary nav__button"
+                                onClick={handleClick}
+                                disabled={showModal}
+                            >
+                                Log in
+                            </button>
                         </li>
-                        {/* <li className="nav__item">About</li> */}
-                        <li className="nav__item">
-                            {authToken && (
-                                <Link to="/dashboard">Dashboard</Link>
-                            )}
-                        </li>
-                        {!authToken && (
+                    )}
+                    {authToken && (
+                        <>
+                            <li>Welcome back, {user && user.name}!</li>
                             <li>
                                 <button
                                     className="button--primary nav__button"
-                                    onClick={handleClick}
-                                    disabled={showModal}
+                                    onClick={logout}
                                 >
-                                    Log in
+                                    Log Out
                                 </button>
                             </li>
-                        )}
-                        {authToken && (
-                            <>
-                                <li>Welcome back, {user && user.name}!</li>
-                                <li>
-                                    <button
-                                        className="button--primary nav__button"
-                                        onClick={logout}
-                                    >
-                                        Log Out
-                                    </button>
-                                </li>
-                            </>
-                        )}
-                    </ul>
-                </nav>
-            </div>
+                        </>
+                    )}
+                </ul>
+            </nav>
+            {/* </div> */}
         </header>
     );
 };
