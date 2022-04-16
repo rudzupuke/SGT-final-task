7571e7912542894687ef4b2670a7e0742c4599be
Mara Braslava <m.braslava@gmail.com>
2022-04-16 21:57:18 +0300

    adding comments

--- frontend/src/components/UtilComponents/ScrollToTop.js
+++ frontend/src/components/UtilComponents/ScrollToTop.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+
+// Prevents default scroll restoration, see https://v5.reactrouter.com/web/guides/scroll-restoration
 export default function ScrollToTop() {
     const { pathname } = useLocation();
 
